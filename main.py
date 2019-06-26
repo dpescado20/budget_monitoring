aee07d1f8aacd199fa8a3c2e0c719cb64c9ba2ba
dpescado <dja.pescado@gmail.com>
2019-06-26 19:38:11 +0800

    test heroku

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # dash library
 import dash
-import dash_core_components as dcc
+# import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
+# from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
-import plotly.graph_objs as go
+# import plotly.graph_objs as go
 
 __navbar = dbc.NavbarSimple(
     children=[],
@@ -20,5 +20,5 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MATERIA])
 app.layout = html.Div([__navbar])
 
 if __name__ == "__main__":
-    # app.run_server()
-    app.run_server(debug=True)
+    app.run_server()
+    # app.run_server(debug=True)
